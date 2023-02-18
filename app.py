0d58d0688f1159e341b9e2f283014c1ce6708e2f
Lucas Bubner <81782264+holo-lb@users.noreply.github.com>
2023-02-18 02:00:19 +0000

    Modified structure for expandability

--- app.py
+++ app.py
@@ -1,118 +1,32 @@
 # A Python GUI calulator built in Tkinter
 # Lucas Bubner, 2023
 
-# Required imports
 import tkinter as tk
+from tkinter import ttk
+from design import create_basic_widgets
 
-# Initialise new calculator container
+
 class Calculator(tk.Tk):
     def __init__(self):
-        # Run Tkinter initialisation
+        # Initialise a new Tkinter class
         super().__init__()
 
-        # Configure display properties including title and grids
+        # Assign window title
         self.title("Calculator")
-        self.display = tk.Entry(self, width=33, borderwidth=5)
-        self.display.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
-        # Map out what buttons to place and where
-        self.buttons = {
-            '1': (1, 0),
-            '2': (1, 1),
-            '3': (1, 2),
-            '4': (2, 0),
-            '5': (2, 1),
-            '6': (2, 2),
-            '7': (3, 0),
-            '8': (3, 1),
-            '9': (3, 2),
-            '0': (4, 0),
-            '+': (5, 0),
-            '-': (5, 1),
-            '*': (5, 2),
-            '/': (5, 3),
-            '=': (6, 1, 2),
-            'C': (4, 1, 2)
-        }
+        # Create a Notebook widget for multiple tabs
+        self.notebook = ttk.Notebook(self.master)
+        self.notebook.pack(fill=tk.BOTH, expand=True)
+    
+        # Create the default tab
+        self.basic_calculator = ttk.Frame(self.notebook)
+        self.notebook.add(self.basic_calculator, text="Basic")
 
-        self.create_buttons()
-
-        # Use running variables to store the current user entries
-        self.current_operation = None
-        self.first_number = None
-
-    def create_buttons(self):
-        # Loop over all buttons in the buttons object
-        for key, value in self.buttons.items():
-            # Determine how much space each button needs
-            row, col = value[:2]
-            colspan = value[2] if len(value) > 2 else 1
-            # Assign a Tkinter dimension to the button
-            button = tk.Button(self, text=key, padx=40, pady=20)
-            button.grid(row=row, column=col, columnspan=colspan)
-            # Configure button actions for each operation and click handling
-            if key in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
-                button.configure(command=lambda x=key: self.number_click(x))
-            elif key == '+':
-                button.configure(command=self.add_click)
-            elif key == '-':
-                button.configure(command=self.sub_click)
-            elif key == '*':
-                button.configure(command=self.mul_click)
-            elif key == '/':
-                button.configure(command=self.div_click)
-            elif key == '=':
-                button.configure(command=self.equal_click)
-            elif key == 'C':
-                button.configure(command=self.clear_click)
-
-    def number_click(self, number):
-        # Get the current display value
-        current = self.display.get()
-        # Clear the entire display
-        self.display.delete(0, tk.END)
-        # Update the display with the old display value plus the recently pressed button
-        self.display.insert(0, str(current) + str(number))
-
-    def clear_click(self):
-        # Clear out the display by deleting everything
-        self.display.delete(0, tk.END)
-
-    def add_click(self):
-        # Obtain current number from display
-        self.first_number = int(self.display.get())
-        # Use an anonymous function to pass for addition
-        self.current_operation = lambda x, y: x + y
-        # Clear display for next number
-        self.display.delete(0, tk.END)
-
-    def sub_click(self):
-        self.first_number = int(self.display.get())
-        # Same as add_click, but with different anonymous functions
-        self.current_operation = lambda x, y: x - y
-        self.display.delete(0, tk.END)
-
-    def mul_click(self):
-        self.first_number = int(self.display.get())
-        self.current_operation = lambda x, y: x * y
-        self.display.delete(0, tk.END)
-
-    def div_click(self):
-        self.first_number = int(self.display.get())
-        self.current_operation = lambda x, y: x / y
-        self.display.delete(0, tk.END)
-
-    def equal_click(self):
-        # Obtain second number after first operation
-        second_number = int(self.display.get())
-        self.display.delete(0, tk.END)
-        # Run the calculation using the function passed for current_operation
-        result = self.current_operation(self.first_number, second_number)
-        # Print the result
-        self.display.insert(0, result)
+        # Add standard widgets to the default tab
+        create_basic_widgets(self, self.basic_calculator)
 
 
-if __name__ == '__main__':
-    # Start Tkinter application
+if __name__ == "__main__":
+    # Initialise new Tkinter application
     app = Calculator()
     app.mainloop()
---
+++ design.py
@@ -0,0 +1,54 @@
+# Tkinter app design module
+
+import tkinter as tk
+from operations import num_c, add_c, sub_c, mul_c, div_c, equal_c, clear_c
+
+# Function to create the standard 0 to 9, clear, equal, and arithmatic operations design
+def create_basic_widgets(instance, target):
+    # Dictionary of buttons to map with standard grid positions
+    buttons = {
+        '1': (3, 0),
+        '2': (3, 1),
+        '3': (3, 2),
+        '4': (2, 0),
+        '5': (2, 1),
+        '6': (2, 2),
+        '7': (1, 0),
+        '8': (1, 1),
+        '9': (1, 2),
+        '0': (4, 0),
+        '+': (1, 3),
+        '-': (2, 3),
+        '*': (3, 3),
+        '/': (4, 3),
+        '=': (4, 2),
+        'C': (4, 1)
+    }
+
+    # Add the entry widget to display the calculation
+    instance.entry = tk.Entry(target, width=25)
+    instance.entry.grid(row=0, column=0, columnspan=4, pady=5)
+
+    # Add the number buttons by looping over each button and it's coordinates
+    for key, value in buttons.items():
+        # Unpack the values from the buttons object
+        row, col = value[:2]
+        colspan = value[2] if len(value) > 2 else 1
+        # Construct a button for each pair
+        btn = tk.Button(instance.basic_calculator, text=key, width=5, height=2)
+        btn.grid(row=row, column=col, columnspan=colspan, padx=5, pady=5)
+        # Assign operations to each button
+        if key in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            btn.configure(command=lambda x=key: num_c(x))
+        elif key == '+':
+            btn.configure(command=add_c())
+        elif key == '-':
+            btn.configure(command=sub_c())
+        elif key == '*':
+            btn.configure(command=mul_c())
+        elif key == '/':
+            btn.configure(command=div_c())
+        elif key == '=':
+            btn.configure(command=equal_c())
+        elif key == 'C':
+            btn.configure(command=clear_c())
