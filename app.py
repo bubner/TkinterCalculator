345ba71d6a9dfb0516c60dbc41f0e12bbb0e9bd7
Lucas Bubner <lkbubner@proton.me>
2023-02-18 21:33:44 +1030

    Revert "Revert "Revert failed attempt to Revert Revert "Attempt to fix interopability"""
    This reverts commit 09f473cef443dd8392b6f139147159cb9f9b460a.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class Calculator(tk.Tk):
         self.title("Calculator")
 
         # Change favicon
-        self.iconphoto(False, tk.PhotoImage("./assets/icon.png"))
+        self.iconbitmap(r"./assets/favicon.ico")
 
         # Prevent resizing
         self.resizable(False, False)
